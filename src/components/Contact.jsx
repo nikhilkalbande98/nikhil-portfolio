403df112daef8cedaa0bb7ae76348a20dde30925
nikhil kalbande <nikhilkalbande@nikhils-MacBook-Air.local>
2024-09-29 22:40:44 +0530

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
       whileInView={{opacity:1,y:0}}
       initial={{opacity:0,y:-100}}
       transition={{duration:0.5}}
-      className="my-10 text-center text-4xl">Get In Touch
+      className="my-10 text-center text-4xl">Contact Me
       </motion.h2>
       <div className="text-center tracking-tighter">
         <motion.p 
